5f7d5d71ba7a578b3fcdb357801d0f28321d1809
최승원 <alxndr@marketboro.com>
2022-11-16 16:34:18 +0900

    Book Entity

--- src/main/java/me/alxndr/springtest/domain/Book.java
+++ src/main/java/me/alxndr/springtest/domain/Book.java
@@ -1,5 +1,29 @@
 package me.alxndr.springtest.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(length = 50, nullable = false)
+	private String title;
+
+	@Column(length = 20, nullable = false)
+	private String author;
 }
