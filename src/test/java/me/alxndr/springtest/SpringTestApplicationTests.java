77a0bf02d5bdc7bcb49d03c44bc0c329023bab24
최승원 <alxndr@marketboro.com>
2022-11-16 16:09:26 +0900

    initial commit

---
+++ src/test/java/me/alxndr/springtest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alxndr.springtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
